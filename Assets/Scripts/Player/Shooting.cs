5b4a22faa776c8d0d24c36087a630037e02f5523
Launaft <xgamergame8@gmail.com>
2024-09-22 22:27:07 +0700

    add ammo count change on UI
    Add script AmmoCountChange
    Add UnityEvent OnAmmoAmountChanged

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -1,44 +1,58 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Shooting : MonoBehaviour
 {
+    public UnityEvent<int, int> OnAmmoAmountChanged;
+
     [SerializeField] private Camera _mainCamera;
     [SerializeField, Range(0.1f, 30f)] private float _impulse = 2.4f;
     [SerializeField, Range(0.1f, 30f)] private float _shootDelay = 5f;
-    [SerializeField, Range(1, 100)] private int _ammoAmount = 30;
+    [SerializeField, Range(1, 100)] private int _maxAmmoAmount = 30;
 
     private int _currentAmmoAmount;
     private float _timeBetweenShoots = 0f;
 
-    private void Start() => _currentAmmoAmount = _ammoAmount;
+    private void Start() => _currentAmmoAmount = _maxAmmoAmount;
 
     private void Update()
     {
+        int previousAmmoAmount = _currentAmmoAmount;
+
         _timeBetweenShoots -= Time.deltaTime;
 
         if (Input.GetMouseButton(0) && _timeBetweenShoots < 0 && _currentAmmoAmount > 0)
+            Shot();
+
+        if (Input.GetKeyDown(KeyCode.R) && _currentAmmoAmount < _maxAmmoAmount)
+            Reload();
+
+        if (previousAmmoAmount != _currentAmmoAmount)
+            OnAmmoAmountChanged?.Invoke(_currentAmmoAmount, _maxAmmoAmount);
+    }
+
+    private void Shot()
+    {
+        Ray ray = new Ray(_mainCamera.transform.position, _mainCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
         {
-            Ray ray = new Ray(_mainCamera.transform.position, _mainCamera.transform.forward);
-            RaycastHit hit;
+            if (hit.transform.CompareTag("Target"))
+                hit.transform.GetComponent<TargetHit>().targetHit(transform.position, hit.point, _impulse);
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.CompareTag("Target"))
-                    hit.transform.GetComponent<TargetHit>().targetHit(transform.position, hit.point, _impulse);
-                Debug.Log(hit.transform);
-                ImpactEffect impactEffect = hit.transform.GetComponent<ImpactEffect>();
-                Debug.Log(impactEffect);
-                if (impactEffect != null)
-                    impactEffect.play(hit.point, hit.normal);
-            }
+            ImpactEffect impactEffect = hit.transform.GetComponent<ImpactEffect>();
 
-            _currentAmmoAmount--;
-            _timeBetweenShoots = _shootDelay;
+            if (impactEffect != null)
+                impactEffect.play(hit.point, hit.normal);
         }
 
-        if (Input.GetKeyDown(KeyCode.R) && _currentAmmoAmount < _ammoAmount)
-        {
-            _currentAmmoAmount = _ammoAmount;
-        }
+        _currentAmmoAmount--;
+        _timeBetweenShoots = _shootDelay;
+    }
+
+    private void Reload()
+    {
+        _currentAmmoAmount = _maxAmmoAmount;
     }
 }
---
+++ Assets/Scripts/UI/AmmoCountChanger.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+
+public class AmmoCountChanger : MonoBehaviour
+{
+    private TMP_Text _ammoCountText;
+    public void ChangeAmmoAmount(int currentAmmoAmount, int maxAmmoAmount) => _ammoCountText.text = $"{currentAmmoAmount} / {maxAmmoAmount}";
+    private void Awake() => _ammoCountText = GetComponent<TMP_Text>();
+}
